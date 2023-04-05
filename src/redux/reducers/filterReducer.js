55c6af496eb067a740295105f781d0868b5ac8df
mhk98 <mohsinkabirseo@gmail.com>
2023-04-05 15:14:38 +0600

    react thunk

---
+++ src/redux/reducers/filterReducer.js
@@ -0,0 +1,28 @@
+import { TOGGLE_BRAND, TOGGLE_STOCK } from "../actionTypes/actionTypes"
+
+export const initialState = {
+    filters: {
+        brands: [],
+        stock: false
+    }
+}
+
+export const filterReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case TOGGLE_BRAND:
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    brands: [...state.filters.brands, action.payload]
+                }
+            };
+
+
+        case TOGGLE_STOCK:
+            return {};
+        default:
+            return state
+    }
+}
