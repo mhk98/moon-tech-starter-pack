22de74a84e7bee270a0c6d8422aa5650c1a954f3
mhk98 <mohsinkabirseo@gmail.com>
2023-04-08 22:46:11 +0600

    6th video

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productReducer from "./reducers/productReducer";
 import rootReducer from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, composeWithDevTools());
+// composeWithDevTools()
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(cartCounter, logger, thunk))
+);
 
 export default store;
